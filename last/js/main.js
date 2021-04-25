84083660a54f3e6ae814ff2e450c2fe56fd79ae2
Yotam Ben porat <66630222+yotam-bp@users.noreply.github.com>
2021-04-25 08:28:22 +0300

    updated

---
+++ last/js/main.js
@@ -0,0 +1,174 @@
+'use strict'
+var isDragging = false
+var gCanvas;
+var gCtx;
+
+function onInit() {
+    gCanvas = document.getElementById('my-canvas')
+    gCtx = gCanvas.getContext('2d')
+    renderGallery(getImgs())
+}
+
+function drawImg(imgId) {
+    const img = new Image()
+    img.src = `img/meme-img/${imgId}.jpg`;
+    img.onload = () => {
+        if (window.innerWidth <= 500) {
+            gCanvas.width = 300;
+            gCanvas.height = 300;
+        }
+        else {
+            gCanvas.width = 500;
+            gCanvas.height = 500;
+        }
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+        drawText()
+    }
+}
+
+function onTextEdit(TxtInput) {
+    updateText(TxtInput)
+    renderCanvas()
+}
+
+function onChangeTxtsize(val) {
+    changeTxtsize(val)
+    renderCanvas()
+}
+
+function onMoveLine(val) {
+    moveLine(val)
+    renderCanvas()
+}
+
+function onAddLine() {
+    addLine()
+    renderCanvas()
+}
+
+function onDeleteLine() {
+    deleteLine()
+    renderCanvas()
+}
+
+function onSwitchLines() {
+    switchLines()
+    renderCanvas()
+}
+
+function onChangeAlign(val) {
+    ChangeAlign(val)
+    renderCanvas()
+}
+
+function onChangeFont(val) {
+    changeFont(val)
+    renderCanvas()
+}
+
+function onChangeFontColor(val) {
+    changeFontColor(val)
+    renderCanvas()
+}
+
+function onChangeBorderColor(val) {
+    changeBorderColor(val)
+    renderCanvas()
+}
+
+function downloadImg(elLink) {
+    var imgContent = gCanvas.toDataURL("image/png")
+    elLink.href = imgContent
+}
+
+function onSaveMeme() {
+    saveMeme(gCanvas)
+    showSavedMemes()
+    clearCanvas()
+}
+
+function onEditImg(meme) {
+    let currMeme = getSavedMemeById(meme)
+    drawImg(currMeme.data.selectedImgId)
+    drawText(currMeme.data.lines)
+    showEditor()
+}
+
+function renderCanvas() {
+    let currMeme = getMeme()
+    drawImg(currMeme.selectedImgId)
+}
+
+function onClickImg(imgId) {
+    showEditor()
+    _createMeme(imgId)
+    renderCanvas()
+}
+
+function drawText() {
+    let gMeme = getMeme()
+    gMeme.lines.forEach(line => {
+        gCtx.strokeStyle = line.borderColor
+        gCtx.fillStyle = line.fontColor
+        gCtx.direction = line.align
+        gCtx.font = line.size + 'px ' + line.fontFamily
+        gCtx.textAlign = line.align
+        gCtx.fillText(line.txt, line.x, line.y)
+        gCtx.strokeText(line.txt, line.x, line.y)
+        document.querySelector('.meme-txt').placeholder = `${gMeme.lines[gMeme.selectedLineIdx].txt}`
+    })
+}
+
+function renderGallery(images) {
+    showGallery()
+    let elheader = `<h2>Choose your favorite Meme</h2>`
+    let strHtml = images.map((img) => {
+        return `<img class="img-box" src="${img.url}" onclick="onClickImg(${img.id})">`;
+    }).join('');
+    document.querySelector('.grid-head').innerHTML = elheader;
+    document.querySelector('.img-container').innerHTML = strHtml;
+}
+
+
+function renderSavedMemes() {
+    const memes = loadMemes()
+    console.log('memes', memes)
+    if (!memes) document.querySelector('.grid-head').innerHTML = '<h2>Make Your First Meme</h2>'
+    else {
+        let strHtml = memes.map((meme) => {
+            return `<img class="meme-box" src="${meme.url}" onclick="onEditImg('${meme.id}')">`;
+        }).join('');
+        document.querySelector('.save-memes-container').innerHTML = strHtml
+    }
+}
+
+function onSearchImg(text) {
+    if (text.value === '') {
+        let gallery = getImgs();
+        renderGallery(gallery)
+        return
+    }
+    let img = getSearchImage(text);
+    renderGallery(img)
+}
+
+function showGallery() {
+    document.querySelector('.edit-memes-container').style.display = 'none'
+    document.querySelector('.img-container').style.display = 'grid'
+    document.querySelector('.save-memes-container').style.display = 'none'
+    document.querySelector('.grid-head').innerHTML = `<h2>Choose your favorite Meme</h2>`
+}
+function showEditor() {
+    document.querySelector('.img-container').style.display = 'none'
+    document.querySelector('.edit-memes-container').style.display = 'flex'
+    document.querySelector('.save-memes-container').style.display = 'none'
+    document.querySelector('.grid-head').innerHTML = `<h2>Edit your Meme</h2>`
+}
+
+function showSavedMemes() {
+    document.querySelector('.grid-head').innerHTML = `<h2>Your saved Memes</h2>`
+    document.querySelector('.edit-memes-container').style.display = 'none'
+    document.querySelector('.img-container').style.display = 'none'
+    document.querySelector('.save-memes-container').style.display = 'grid'
+    renderSavedMemes()
+}
---
+++ last/js/meme-service.js
@@ -0,0 +1,226 @@
+'use strict'
+
+const MEMES_KEY = 'saved memes';
+var gSavedMemes = [];
+var gMeme;
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
+var gCurrLine 
+
+var gImgs = [
+    { id: 1, url: 'img/meme-img/1.jpg', keywords: ['politics'] },
+    { id: 2, url: 'img/meme-img/2.jpg', keywords: ['animals', 'cute'] },
+    { id: 3, url: 'img/meme-img/3.jpg', keywords: ['animals', 'cute', 'baby'] },
+    { id: 4, url: 'img/meme-img/4.jpg', keywords: ['animals', 'cute'] },
+    { id: 5, url: 'img/meme-img/5.jpg', keywords: ['baby', 'cute'] },
+    { id: 6, url: 'img/meme-img/6.jpg', keywords: ['tv', 'funny'] },
+    { id: 7, url: 'img/meme-img/7.jpg', keywords: ['cute', 'baby', 'funny'] },
+    { id: 8, url: 'img/meme-img/8.jpg', keywords: ['tv', 'funny'] },
+    { id: 9, url: 'img/meme-img/9.jpg', keywords: ['cute', 'baby', 'funny'] },
+    { id: 10, url: 'img/meme-img/10.jpg', keywords: ['politics', 'funny'] },
+    { id: 11, url: 'img/meme-img/11.jpg', keywords: ['sports','funny'] },
+    { id: 12, url: 'img/meme-img/12.jpg', keywords: ['tv'] },
+    { id: 13, url: 'img/meme-img/13.jpg', keywords: ['movies'] },
+    { id: 14, url: 'img/meme-img/14.jpg', keywords: ['movies'] },
+    { id: 15, url: 'img/meme-img/15.jpg', keywords: ['movies'] },
+    { id: 16, url: 'img/meme-img/16.jpg', keywords: ['tv', 'funny'] },
+    { id: 17, url: 'img/meme-img/17.jpg', keywords: ['politics'] },
+    { id: 18, url: 'img/meme-img/18.jpg', keywords: ['tv','kids'] },
+
+];
+
+
+
+function _createMeme(imgId) {
+    if(window.innerWidth <= 500){
+        var x = gCanvas.width / 2;
+        var y = gCanvas.height / 2 
+    }else {
+        var x = 250;
+        var y = 50; 
+    }
+    gMeme = {
+        id: makeId(),
+        selectedImgId: imgId,
+        selectedLineIdx: 0,
+        lines:
+            [{
+                txt: 'Add text',
+                size: 40,
+                borderColor: '#000000',
+                fontFamily: 'impact',
+                fontColor: '#ffffff',
+                align: 'center',
+                x,
+                y
+            }]
+        }
+}
+
+function getImgs() {
+    return gImgs;
+}
+
+function getMeme() {
+ return gMeme;
+}
+
+
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
+}
+
+function getImgByIdx() {
+    return gImgs.findIndex(img => gMeme.selectedImgId === img.id);
+}
+
+function getSavedMemeById(memeId) {
+    let memes = loadMemes()
+    let meme = memes.find(meme => {
+        return memeId === meme.id;
+    })
+     return meme;
+}
+
+function updateText(TxtInput) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = TxtInput
+}
+
+function changeTxtsize(val) {
+    gMeme.lines[gMeme.selectedLineIdx].size += val
+}
+
+function moveLine(val) {
+    gMeme.lines[gMeme.selectedLineIdx].y += val
+}
+
+function ChangeAlign(val) {
+    gMeme.lines[gMeme.selectedLineIdx].align = val
+}
+
+function addLine() {
+    if (gMeme.lines.length ===0) {
+        var x = 250
+        var y = 50
+        if(window.innerWidth <= 500){
+            var x = gCanvas.width / 2;
+            var y = gCanvas.height / 2 
+        }
+    }
+    else if (gMeme.lines.length === 1) {
+        var x = 250
+        var y = 450
+        if(window.innerWidth <= 500){
+            var x = gCanvas.width / 2;
+            var y = 250 
+        }
+    }
+    else {
+        var x = 250
+        var y = 250
+        if(window.innerWidth <= 500){
+            var x = gCanvas.width / 2;
+            var y = 150 
+        }
+    }
+    gMeme.selectedLineIdx++
+    let line = {
+        txt: 'Add text',
+        size: 40,
+        borderColor: 'black',
+        fontFamily: 'impact',
+        fontColor: 'white',
+        align: 'center',
+         x, 
+         y 
+    };
+    gMeme.lines.push(line)
+}
+
+
+
+function deleteLine() {
+    if (gMeme.lines.length === 0) return
+    gMeme.lines.splice([gMeme.selectedLineIdx], 1)
+    gMeme.selectedLineIdx--
+}
+
+function changeFontColor(color) {
+    gMeme.lines[gMeme.selectedLineIdx].fontColor = color
+}
+
+function changeBorderColor(color) {
+    gMeme.lines[gMeme.selectedLineIdx].borderColor = color
+}
+
+function changeFont(font) {
+    gMeme.lines[gMeme.selectedLineIdx].fontFamily = font
+}
+
+function switchLines() {
+    if (gMeme.lines.length === 0) return;
+    if (gMeme.selectedLineIdx + 1 === gMeme.lines.length) gMeme.selectedLineIdx = 0;
+    else gMeme.selectedLineIdx++;
+    gMeme.lines[gMeme.selectedLineIdx].borderColor = 'red'
+
+}
+
+// upload to facebook
+function uploadImg(elForm, ev) {
+    ev.preventDefault();
+    document.getElementById('imgData').value = gCanvas.toDataURL("image/jpeg");
+
+    function onSuccess(uploadedImgUrl) {
+        uploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+        document.querySelector('.btn-share').innerHTML = `
+        <a class="btn" href="https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
+           Share   
+        </a>`
+    }
+    let inputVal = elForm.querySelector('input').value
+    doUploadImg(elForm, onSuccess, inputVal);
+}
+
+function doUploadImg(elForm, onSuccess) {
+    var formData = new FormData(elForm);
+    console.log('doUploadImg -> formData', formData)
+    fetch('//ca-upload.com/here/upload.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(function (res) {
+            return res.text()
+        })
+        .then(onSuccess)
+        .catch(function (err) {
+            console.error(err)
+        })
+}
+
+function saveMeme(gCanvas) {
+    let currMeme = {
+        id: makeId(),
+        data: gMeme,
+        url: gCanvas.toDataURL('image/jpeg')
+    }
+    gSavedMemes.push(currMeme);
+    saveToStorage(MEMES_KEY, gSavedMemes)
+
+}
+
+function loadMemes() {
+    gSavedMemes = loadFromStorage(MEMES_KEY)
+    if (!gSavedMemes || gSavedMemes.length === 0) return;
+    return gSavedMemes;
+}
+
+function getSearchImage(text) {
+    return gImgs.filter(img => {
+        return img.keywords.filter(keyword => {
+            return keyword.includes(text)
+        }).length
+    })
+}
+
+
+
+
